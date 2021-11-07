ab3105250fa4d91d9fa2667c2f77f033d43dfec0
Kate Kiatsiri <keidsiri@gmail.com>
2021-11-07 01:31:26 -0700

    Change from scheck boxes to select boxes on html and add business codes for calculate price

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,18 @@
 // Business Logic
 
-function Pizza (size, sauce, topping, delivery) {
+function Pizza (size, sauce, topping1, topping2, topping3, delivery) {
   this.size = size;
   this.sauce = sauce;
-  this.topping = topping;
+  this.topping1 = topping1;
+  this.topping2 = topping2;
+  this.topping3 = topping3;
   this.order = delivery;
   this.price = 10;
 }
 
-// Pizza.prototype.priceCalculate = function() {
-//   this.price = 9.99;
-//   if ( this.size === "X-large") {
-//     this.price += 6;
-//   } if (this.size === "Large") {
-//     this.price += 4;
-//   } if (this.size = "Medium") {
-//     this.price += 2;
-//   }
-// }
-
 Pizza.prototype.priceCalculate = function() {
-  let totalTopping = 0;
   
-  if ( this.size === "X-large") {
+  if ( this.size === "Extra-large") {
     this.price += 4;
   } if (this.size === "Large") {
     this.price += 3;
@@ -38,61 +28,39 @@ Pizza.prototype.priceCalculate = function() {
   } if (this.order) {
     this.price += 5;
   }
-
-    this.topping.forEach(function(topping) {
-      switch(topping) {
-        case ("Pepperoni"):
-          totalTopping += 1;
-          break;
-        case ("Italian sausage"):
-          totalTopping += 1;
-          break;
-        case ("Bacon"):
-          totalTopping += 1;
-          break;
-        case ("Mushroom"):
-          totalTopping += 1;
-          break;
-        case ("Onion"):
-          totalTopping += 1;
-          break;
-        case ("Bell Pepper"):
-          totalTopping += 1;
-          break;
-        case ("Pineapple"):
-          totalTopping += 1;
-          break;
-        default:
-        break;
-      }
-    });
-  return this.price + totalTopping;
+  if (this.topping1) {
+    this.price += 0.5;
+  }
+  if (this.topping2 ) {
+    this.price += 0.5;
+  }
+  if (this.topping3 ) {
+    this.price += 0.5;
+  }
+  return this.price;
 }
 
-let test1 = new Pizza("X-large", "BBQ", ["Onion", "Bacon"])
-test1.priceCalculate();
 
-//select box for size : Small, medium, large, x-large
-//select box for sauce : classic tomato, white garlic, pesto , BBQ
-//topping : [pepperoni ,italian sausage, bacon, mushroom, onion, bell pepper, pineapple]
-
-
-
-
+// let test1 = new Pizza("X-large", "BBQ", ["Onion", "Bacon"])
+// test1.priceCalculate();
 
 // UI logic
+
 function displayPrice(pizza) {
   $(".result").show();
-  $("#your-size").text(pizza.size);
-  $("#your-sauce").text(pizza.sauce);
+  $("#your-size").text("Size : " + pizza.size);
+  $("#your-sauce").text("Sauce : " + pizza.sauce);
+  $("#your-topping1").text("Topping: " + pizza.topping1);
+  $("#your-topping2").text(pizza.topping2);
+  $("#your-topping3").text(pizza.topping3);
   if (pizza.order) {
     $("#your-delivery").text("We will deliver this pizza to you!");
   } else {
-    $("#your-delivery").text("YOur pizza will be ready to pick up in 15 minutes!");
+    $("#your-delivery").text("Your pizza will be ready to pick up in 15 minutes!");
 
   }
   pizza.priceCalculate();
-  $("#your-price").text("$" + pizza.price.toFixed(2));
+  $("#your-price").text("Total is $" + pizza.price.toFixed(2));
 }
 
 $(document).ready(function() {
@@ -100,13 +68,16 @@ $(document).ready(function() {
     event.preventDefault();
     const size = $("#size option:selected").val();
     const sauce = $("#sauce option:selected").val();
-    const topping = $(".topping option:selected").html(); 
+    const topping1 = $("#topping1 option:selected").html();
+    const topping2 = $("#topping2 option:selected").html();
+    const topping3 = $("#topping3 option:selected").html();
 
     let delivery = false;
-    if ($("input:checked").val()) {
+    if ($("#delivery:checked").val()) {
       delivery = true;
     }
-    let myPizza = new Pizza (size ,sauce ,topping, delivery);
+    let myPizza = new Pizza (size ,sauce ,topping1, topping2, topping3, delivery);
     displayPrice(myPizza);
   });
 });
+
